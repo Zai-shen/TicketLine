4ff00818aa416dfcea24154b8089cc1fcb1f96b5
Dominik Süß <dominik@suess.wtf>
2020-05-24 19:03:11 +0200

    #44 add testcases for new mapper methods

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/controller/mapper/UserMapperTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/controller/mapper/UserMapperTest.java
@@ -15,4 +15,16 @@ public class UserMapperTest {
         assertThat(userMapper.toEntity(DTOTestObjectFactory.getUserDTO()))
             .isEqualToIgnoringGivenFields(DomainTestObjectFactory.getUser(), "role");
     }
+
+    @Test
+    public void testToDTO() {
+        assertThat(userMapper.toDto(DomainTestObjectFactory.getUser()))
+            .isEqualToIgnoringGivenFields(DTOTestObjectFactory.getUserDTO(), "role", "login");
+    }
+
+    @Test
+    public void testUpdateToEntity() {
+        assertThat(userMapper.toEntity(DTOTestObjectFactory.getUserUpdateDTO()))
+            .isEqualToIgnoringGivenFields(DomainTestObjectFactory.getUser(), "id","email","password","role", "login");
+    }
 }
