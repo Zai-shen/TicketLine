efc7262dcde58a8a8aed1ae9996cc43af62b5210
Miran Jank <e1526438@mail.student.tuwien.ac.at>
2020-05-25 17:11:02 +0200

    #40 News anzeigen QoL.

--- frontend/src/app/components/news/news.component.ts
+++ frontend/src/app/components/news/news.component.ts
@@ -23,4 +23,24 @@ export class NewsComponent implements OnInit {
   isAdmin(): boolean {
     return this.authService.isAdminLoggedIn();
   }
+
+  computeMinutes(date: string) {
+    const eventStartTime = new Date(date);
+    const eventEndTime = new Date();
+    const duration = eventEndTime.valueOf() - eventStartTime.valueOf();
+    const months = Math.floor(duration / 2628000000) % 12;
+    const days = Math.floor(duration / 86400000) % 31;
+    const hours = Math.floor(duration / 3600000) % 24; // 1 Hour = 36000 Milliseconds
+    const minutes = Math.floor((duration % 3600000) / 60000) % 60; // 1 Minute = 60000 Milliseconds
+    // const seconds = Math.floor(((duration % 360000) % 60000) / 1000) % 60; // 1 Second = 1000 Milliseconds
+    let time;
+    if (minutes <= 2) {
+      time = 'gerade eben';
+    } else {
+      time = 'vor ' + (months > 0 ? months + ' Monaten ' : '') + (days > 0 ? days + ' Tagen ' : '') +
+        (hours > 0 ? hours + ' Stunden ' : '') + (minutes > 0 ? minutes + ' Minuten' : '');
+    }
+    return time;
+  }
+
 }
