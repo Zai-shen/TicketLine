f79b15f3ddc7440fb0c8d2b84c1417abddd9c5e3
Stefanie Muehlehner <e01634027@student.tuwien.ac.at>
2020-06-10 20:01:43 +0200

    #62 price search

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/SearchPerformanceSpecification.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/SearchPerformanceSpecification.java
@@ -3,8 +3,11 @@ package at.ac.tuwien.sepm.groupphase.backend.repository;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Performance;
 import at.ac.tuwien.sepm.groupphase.backend.entity.SearchPerformance;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Seat;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.*;
 import java.sql.Time;
 import java.time.LocalDate;
@@ -14,6 +17,9 @@ import java.time.ZoneOffset;
 import java.util.List;
 
 public class SearchPerformanceSpecification implements Specification<Performance> {
+
+    @PersistenceContext
+    EntityManager entityManager;
 
     private static final int MINUTES_OFFSET = 30;
     private SearchPerformance filter;
@@ -44,12 +50,27 @@ public class SearchPerformanceSpecification implements Specification<Performance
                 startTime, endTime);
             searchCriteria.add(timeSearch);
         }
-
         if (filter.getEvent() != null) {
-            Join<Performance,Event> eventJoin = root.join("event");
+            Join<Performance, Event> eventJoin = root.join("event");
             Expression<Boolean> eventSearch = criteriaBuilder.like(criteriaBuilder.lower(eventJoin.get("title")),
                 "%" + filter.getEvent().toLowerCase() + "%");
             searchCriteria.add(eventSearch);
+        }
+        if (filter.getPrice() != null) {
+            Join<Performance, Seat> seatJoin = root.join("location").join("seatmap")
+                .join("seatGroupAreas").join("seats");
+
+            Subquery<Performance> subquery = criteriaQuery.subquery(Performance.class);
+            subquery.select(root.join("location").join("seatmap")
+                .join("seatGroupAreas").join("seats"));
+            subquery.where(criteriaBuilder.lessThan(seatJoin.get("price"), filter.getPrice().doubleValue()));
+            Expression<Boolean> test = criteriaBuilder.exists(subquery);
+
+
+            Expression<Boolean> priceSearch = criteriaBuilder.between(seatJoin.get("price"),
+                filter.getPrice().doubleValue()-10, filter.getPrice().doubleValue()+10);
+
+            searchCriteria.add(priceSearch);
         }
         return predicate;
     }
