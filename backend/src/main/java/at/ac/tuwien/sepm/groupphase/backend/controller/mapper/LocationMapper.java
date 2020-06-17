d4dcf0bb5af926f79ddf149a09d10afda220ba1e
Dominik Süß <dominik@suess.wtf>
2020-06-17 16:25:46 +0200

    #89 realistic seatmap

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/controller/mapper/LocationMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/controller/mapper/LocationMapper.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepm.groupphase.backend.controller.mapper;
 
 import at.ac.tuwien.sepm.groupphase.backend.dto.*;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Location;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Seatmap;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Named;
@@ -19,4 +20,5 @@ public interface LocationMapper {
 
     List<LocationDTO> locationsToLocationsDto(List<Location> locations);
 
+    Seatmap fromDto(SeatmapDTO seatmapDTO);
 }
