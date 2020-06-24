79a40325af30317e869dbec7c581e969a4404907
Miran Jank <e1526438@mail.student.tuwien.ac.at>
2020-06-24 13:51:15 +0200

    #58 Added more tests, updated existing ones.

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/controller/NewsControllerIntegrationTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/controller/NewsControllerIntegrationTest.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -27,15 +30,41 @@ public class NewsControllerIntegrationTest {
 
     @Test
     public void testReturn404OnInvalidNews() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/news/1337"))
+        mockMvc.perform(securityHelper.withUserAuthentication(MockMvcRequestBuilders.get("/news/1337")))
             .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
     @Test
-    public void testGetNewsListHasTotalCountHeader() throws Exception {
+    public void testReturn400CodeErrorResponseOnInvalidJSONFormat() throws Exception {
+        String invalidJson = "{";
         mockMvc.perform(
-            MockMvcRequestBuilders.get("/news"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.header().exists("X-Total-Count"));
+            MockMvcRequestBuilders.post("/news").content(invalidJson).contentType(MediaType.APPLICATION_JSON))
+
+            .andExpect(MockMvcResultMatchers.status().isBadRequest())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.type").value("WARN"))
+            .andExpect(jsonPath("$.message").isNotEmpty());
     }
+
+    @Test
+    public void testCreateNewsReturn422CodeErrorResponseOnDTOValidationFailed() throws Exception {
+        String jsonWithNulls = "{ \"title\": \"A title\", \"summary\": \"A summary\", \"content\": \"A small content\", \"author\": null}";
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/user").content(jsonWithNulls).contentType(MediaType.APPLICATION_JSON))
+
+            .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.type").value("WARN"))
+            .andExpect(jsonPath("$.message").isNotEmpty());
+    }
+
+    @Test
+    public void testGetNewsReturn403CodeErrorResponseOnInsufficientPermissions() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/news/1337"))
+            .andExpect(MockMvcResultMatchers.status().isForbidden())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.type").value("WARN"))
+            .andExpect(jsonPath("$.message").isNotEmpty());
+    }
+
 }
--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/service/NewsServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/service/NewsServiceTest.java
@@ -38,11 +38,12 @@ public class NewsServiceTest {
 
     @Test
     public void testPublishNewsWithValidInput() {
-        //final News news = DomainTestObjectFactory.getNews();
-        //
-        //newsService.publishNews(news);
-        //
-        //verify(newsRepository, times(1)).saveAndFlush(news);
+        final News news = DomainTestObjectFactory.getNews();
+        when(newsRepository.saveAndFlush(news)).thenReturn(news);
+
+        newsService.publishNews(news);
+
+        verify(newsRepository, times(1)).saveAndFlush(news);
     }
 
     @Test
